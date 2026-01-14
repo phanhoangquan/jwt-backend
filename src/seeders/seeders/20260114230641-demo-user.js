34654379d9a659514597f2bc5d07b7e27500a348
phan_hoang_quan <phanhoangquan123@gmail.com>
2026-01-15 06:15:50 +0700

    ORM Structure - Model/Migrations/Seeder

---
+++ src/seeders/seeders/20260114230641-demo-user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+   up: async (queryInterface, Sequelize) => {
+      /**
+       * Add seed commands here.
+       *
+       * Example:
+       * await queryInterface.bulkInsert('People', [{
+       *   name: 'John Doe',
+       *   isBetaMember: false
+       * }], {});
+       */
+
+      await queryInterface.bulkInsert(
+         'Users',
+         [
+            {
+               email: 'John Doe',
+               password: 'password123',
+               username: 'johndoe',
+            },
+            {
+               email: 'John Doe2',
+               password: 'password123',
+               username: 'johndoe',
+            },
+            {
+               email: 'John Doe3',
+               password: 'password123',
+               username: 'johndoe',
+            },
+         ],
+         {},
+      );
+   },
+
+   down: async (queryInterface, Sequelize) => {
+      /**
+       * Add commands to revert seed here.
+       *
+       * Example:
+       * await queryInterface.bulkDelete('People', null, {});
+       */
+   },
+};
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import configViewEngine from './config/viewEngine';
 import initWebRoutes from './routes/web';
 import bodyParser from 'body-parser';
+// import connection from './config/connectDB';
 
 require('dotenv').config();
 
@@ -14,6 +15,9 @@ configViewEngine(app);
 //config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Test the connection to the database
+// connection();
 
 //Init web routes
 initWebRoutes(app);
